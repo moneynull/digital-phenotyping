3ed6a45fffb3588c00b9746059697ab668101531
Marvin3Benz <1074965720@qq.com>
2022-04-10 17:31:18 +1000

    Fixed button style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,17 +17,6 @@ function App() {
           <div></div>
           <Link to='/loginpage'>Login</Link>
         </nav>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- frontend/src/pages/Login/Loginpage.jsx
+++ frontend/src/pages/Login/Loginpage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export default function Loginpage() {
-  return  <MainContainer>Loginpage
-            <div><Link to="/homepage"><button>Go to Home Page </button></Link></div>
-          </MainContainer>;
+  return (
+    <MainContainer>
+      Loginpage
+      <div>
+        <Link to='/homepage'>
+          <Button variant='contained'>Go to Home Page </Button>
+        </Link>
+      </div>
+    </MainContainer>
+  );
 }
 
 const MainContainer = styled.div`
