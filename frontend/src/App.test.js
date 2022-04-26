4351d175d5f574913e5708b039165209aae2e0cd
noobShuo <100746235+noobShuo@users.noreply.github.com>
2022-04-26 10:42:57 +1000

    add testcases (#11)

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('snapshot', () => {
+  it('library render', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
+
+it('renders no crashing', () =>{
+  const div = document.createElement('div');
+  //ReactDOM.render(<App />, div);
+} );
+
