35e4a1a115274c5a41dc6a1d981a37b98ad30df2
bing xu <bingxu.au@gmail.com>
2022-09-05 12:15:41 +1000

    Remove duplicate API function

--- backend/django_site/dataServer/views.py
+++ backend/django_site/dataServer/views.py
@@ -12,28 +12,6 @@ from locationServer.views import PreProcessLocation
 
 one_day = 86400000
 
-def extract_twitter_keywords(request):
-    if request.method != 'GET':
-        return HttpResponseBadRequest
-    req = json.loads(request.body.decode().replace("'", "\""))
-    uid = req.get('id')
-    twitter_id = models.TbClient.objects.get(uid=uid).twitter_id
-
-    # the following code is for get twitter id based on twitter username
-    # it will be used in the profile page
-    # ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-    # client = tweepy.Client(bearer_token=bearer_token)
-    # username = "" # Username is what client inputs
-    # twitter_id = client.get_user(username=username).data.id
-    # 
-
-    records = models.TwitterWordCloud.objects.filter(twitter_id=twitter_id).values_list('word','occurance')
-
-    res = {
-        'success': True,
-        'data': dict(list(records))
-    }
-    return HttpResponse(json.dumps(res, cls=DjangoJSONEncoder), content_type='application/json')
 
 def extract_twitter_keywords(request):
     if request.method != 'POST':
