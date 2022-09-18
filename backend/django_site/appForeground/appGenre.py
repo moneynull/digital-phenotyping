cc1303acd88aa805a9f9a403bd8b6eaa27622534
moneynull <76108708+moneynull@users.noreply.github.com>
2022-09-18 18:26:09 +1000

    Be/serializable and token (#79)
    * fix csrf issue
    
    * fix csrf issue
    
    * optimize catogry
    
    * update google play version
    
    * update google play version

--- backend/django_site/appForeground/appGenre.py
+++ backend/django_site/appForeground/appGenre.py
@@ -1,44 +1,39 @@
-from django.core.management.base import BaseCommand, CommandError
-
-from google_play_scraper import app as google_app  # https://pypi.org/project/google-play-scraper/
+import pymysql
+import DB_connection_details
 import json
+from google_play_scraper import app as google_app
+from google_play_scraper.exceptions import NotFoundError
 
-from django_site.appForeground.models import ApplicationsForeground
+conn = pymysql.connect(host=DB_connection_details.host,
+                       user=DB_connection_details.user,
+                       passwd=DB_connection_details.passwd,
+                       db=DB_connection_details.db,
+                       autocommit=True)
 
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+cursor2 = conn.cursor(pymysql.cursors.DictCursor)
+cursor3 = conn.cursor(pymysql.cursors.DictCursor)
 
-#
-# with connection.cursor() as cursor:
-#     cursor.execute("SELECT * FROM applications_foreground WHERE application_category IS NULL")
-#     results = dictfetchall(cursor)
-#     for result in results:
-#         try:
-#             google_info = google_app(
-#                 result['package_name'])  # https://pypi.org/project/google-play-scraper/
-#             if google_info['genre']:
-#         # update application_category cell for this record
-#         except:
-#             pass
-#
-# def dictfetchall(cursor):
-#     # Return all rows from a cursor as a dict
-#     columns = [col[0] for col in cursor.description]
-#     return [
-#         dict(zip(columns, row))
-#         for row in cursor.fetchall()
-#     ]
+cursor.execute("SELECT DISTINCT package_name FROM applications_foreground WHERE category IS NULL")
+app_records = cursor.fetchall()
 
-def updateGenre():
-    genre_result = ApplicationsForeground.objects.filter(application_category__isnull=True)
-    for r in genre_result:
-        try:
-            google_info = google_app(
-                r['package_name'])  # https://pypi.org/project/google-play-scraper/
-            if google_info['genre']:
-                # update application_category cell for this record
-                r.application_category = google_info['genre']
-        except:
-            pass
-    genre_result.objects.bulk_update(genre_result, update_fields=['application_category'])
+for app_record in app_records:
+    cursor2.execute("SELECT category FROM applications_foreground WHERE package_name = '" + app_record[
+        'package_name'] + "' AND category IS NOT NULL LIMIT 1")
+    category_result = cursor2.fetchone()
+    category = None
+    if category_result:
+        category = category_result['category']
 
+if not category:
+    try:
+        google_info = google_app(app_record['package_name'])  # https://pypi.org/project/google-play-scraper/
+        category = google_info['genre']
+    except NotFoundError:
+        category = "None"
 
+if not category:
+    category = "None"
 
+cursor3.execute("UPDATE applications_foreground SET category = '" + category + "' WHERE package_name = '" + app_record[
+    'package_name'] + "' AND category IS NULL")
